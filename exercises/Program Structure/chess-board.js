bfe93662e5af341cd7a86516503d64ba38ed19a5
Igor Martimiano <igor.martimiano@gmail.com>
2018-04-04 20:42:30 -0300

    Added exercises from Program  Structure

---
+++ exercises/Program Structure/chess-board.js
@@ -0,0 +1,15 @@
+let size  = 6,
+    board = '';
+
+for (let i = 1; i <= size; i++) {
+    for(let e = 1; e <= size; e++) {
+        if ((e + i) % 2 === 0) {
+            board += ' ';
+        } else {
+            board += '♚';    
+        }
+    }
+    board += "\n";
+}
+
+console.log(board);
